879fc8e6438e77f14027135f3eef51472ef4e0ba
Sreekanth Reddy <sreekanthiragamreddy@samjain.local>
2017-09-15 10:28:13 -0700

    Initial Commit

---
+++ TabbarAppUICode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabbarAppUICode
+//
+//  Created by Sreekanth Iragam Reddy on 9/15/17.
+//  Copyright © 2017 Sreekanth Iragam Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
